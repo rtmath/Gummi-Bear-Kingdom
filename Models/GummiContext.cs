06f66ee5ea8c546be1421282d13d1fa5aa458bf1
Ryan Mathisen <ryantmathisen@gmail.com>
2017-02-10 07:52:43 -0800

    add initial files and folders

---
+++ Models/GummiContext.cs
@@ -0,0 +1,33 @@
+﻿using GummiBearKingdom.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummiBearKingdom.Models
+{
+    public class GummiContext : DbContext
+    {
+        public GummiContext()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GummiBearKingdom;integrated security=True");
+        }
+
+        public GummiContext(DbContextOptions<GummiContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
