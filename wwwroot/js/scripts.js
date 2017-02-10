d50bf76d65244a586cfe4d53ab391dff03f323a4
Ryan Mathisen <ryantmathisen@gmail.com>
2017-02-10 11:40:14 -0800

    add css, jquery, and styling to Home Index page

---
+++ wwwroot/js/scripts.js
@@ -0,0 +1,29 @@
+﻿function sidebarToggle(string) {
+    $('#' + string + '-content').toggle();
+    $('#' + string).toggle();
+    $('#' + string + '-active').toggle();
+}
+
+$(document).ready(function () {
+
+    $('#sidebar-about-us').click(function () {
+        sidebarToggle("sidebar-about-us");
+    })
+    $('#sidebar-about-us-active').click(function () {
+        sidebarToggle("sidebar-about-us");
+    })
+
+    $('#sidebar-contact').click(function () {
+        sidebarToggle("sidebar-contact");
+    })
+    $('#sidebar-contact-active').click(function () {
+        sidebarToggle("sidebar-contact");
+    })
+
+    $('#sidebar-product').click(function () {
+        sidebarToggle("sidebar-product");
+    })
+    $('#sidebar-product-active').click(function () {
+        sidebarToggle("sidebar-product");
+    })
+})
