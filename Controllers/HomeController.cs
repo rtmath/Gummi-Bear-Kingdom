9dc96462e860c0ce5581aade14e8965ac608cf44
Ryan Mathisen <ryantmathisen@gmail.com>
2017-02-10 16:53:45 -0800

    add About Us and Contact sections with Cupcake Ipsum, adjust css

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,5 +12,15 @@ namespace GummiBearKingdom.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
